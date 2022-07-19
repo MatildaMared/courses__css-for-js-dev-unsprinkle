e5d0ab3c836662fb5e6ea81ba648521899e84acf
Matilda Mared <matildamared@live.se>
2022-07-19 21:35:44 +0200

    add alt texts

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -21,9 +21,12 @@ const Hero = () => {
 					/images/hero-img@3x.jpg 3x
 					"
 				/>
-				<HeroImage src="/images/hero-img.jpg" />
+				<HeroImage
+					src="/images/hero-img.jpg"
+					alt="Tabby cat with blue eyes looking straight at the camera"
+				/>
 			</picture>
-			<Swoop src="/swoop.svg" />
+			<Swoop src="/swoop.svg" alt="" />
 		</Wrapper>
 	);
 };
